307231c8d389ccddca767f5ddfb9527cd16ee2d5
SUNIL KUMAR <sk054985@gmail.com>
2020-03-14 11:46:22 +0530

    Fourth commit

--- JS Bacics/index.js
+++ JS Bacics/index.js
@@ -1,3 +1,6 @@
-<script>
-console.log("Hello World");
-</script>
+function myFunc(b,h){
+    area = b*h;
+    perimeter = 2*(b+h);
+    console.log("Area ="+" "+area , "Perimeter =" + " " +perimeter);
+}
+myFunc(2,3);
