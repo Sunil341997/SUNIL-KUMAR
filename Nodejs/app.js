b3f0d3acb8ae76a19f06c72fd4e4273e067c51cb
SUNIL KUMAR <sk054985@gmail.com>
2020-03-15 12:55:54 +0530

    9th commit

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+//meaning of utf8?
 var readMe = fs.readFileSync('readme.txt', 'utf8');
 fs.writeFileSync('writeMe', readMe);
 
--- Nodejs/stearm.js
+++ Nodejs/stearm.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 //meaning of __dirname?
+//meaning of utf8?
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
 myReadStream.on('data', function(chunk){
     console.log('New chunk received:');
