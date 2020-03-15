59887a92bfcbb5052e2f417cdce26884ebd8dd75
SUNIL KUMAR <sk054985@gmail.com>
2020-03-15 20:20:38 +0530

    15th commit

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -8,5 +8,5 @@ var server = http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/plan'});
     res.end('Hey guys');
 });
-server.listen (3000, '127.0.0.1');
+server.listen(3000, '127.0.0.1');
 console.log('yo guys listening to port 3000');
