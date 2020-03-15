875567773e141ffad2debe0fc3a659195dce191d
SUNIL KUMAR <sk054985@gmail.com>
2020-03-15 13:40:56 +0530

    13th commit

--- npm/moment.js
+++
@@ -1,7 +0,0 @@
-var moment = require("moment");
-var uniq = require("uniq");
-var myDate = new Date();
-var myCoolDate = moment(myDate).format('LL');
-var myList = [1,2,1,1,1,3,4,3,7,8,9,5,5,5];
-var myUniqueList = unique(myList);
-console.log(myUniqueList);
