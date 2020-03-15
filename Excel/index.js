7d9537f8cd52c72e051db7c164ec82ce17fb031f
SUNIL KUMAR <sk054985@gmail.com>
2020-03-15 13:00:50 +0530

    10th commit

---
+++ Excel/index.js
@@ -0,0 +1,21 @@
+var xlsx = require('xlsx');
+var readFile = xlsx.readFile('SampleData.xlsx', {cellDates:true});
+var ws = readFile.Sheets['SalesOrders'];
+var data = xlsx.utils.sheet_to_json(ws);
+var newData = data.map(function(record){
+    var net = record.UnitCost*record.Units;
+    record.Total = net;
+    //not updating in sheet only in console.
+    delete record.Rep2;
+    delete record.Rep3;
+    return record;
+});
+console.log(newData);
+
+var newW = xlsx.utils.book_new();
+//meaning of work book
+var newWS = xlsx.utils.json_to_sheet(newData);
+//meaning of work sheet
+//difference in between those
+xlsx.utils.book_append_sheet(newW, newWS, "New Data");
+xlsx.writeFile(newW, "New Data File.xlsx");
