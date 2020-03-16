4f12575a84bf7fbab1cf528fc96d8cd5a96c0a09
SUNIL KUMAR <sk054985@gmail.com>
2020-03-16 09:28:39 +0530

    15th commit

--- Nodejs/stearm.js
+++ Nodejs/stearm.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 //meaning of __dirname?
 //meaning of utf8?
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+//meaning of data here
 myReadStream.on('data', function(chunk){
     console.log('New chunk received:');
     console.log(chunk);
